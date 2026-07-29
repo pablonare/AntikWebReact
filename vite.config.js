92552fa12ab42cd44a411ee4d95d42785c7c21d7
Pablo Naredo <pnaredobernardos@gmail.com>
2026-07-29 13:57:40 +0800

    Add sitemap generation to Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      Sitemap({
+        hostname: "https://antiksurf.com",
+        dynamicRoutes: ["/", "/surftrips", "/shop", "/events"],
+        basePath: "",
+      }),
+    ],
     base: env.VITE_PUBLIC_BASE || "/",
   };
 });
