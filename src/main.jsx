0ac63da4db4f1281a61c9eccac508314629b0e49
Pablo Naredo <pnaredobernardos@gmail.com>
2026-07-29 13:17:01 +0800

    enable dual hosting: Raiola production and GitHub Pages preview

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,13 @@ import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import ScrollToTop from "./components/scrolltop/ScrollTop.jsx";
 
+const baseUrl = import.meta.env.BASE_URL || "/";
+const routerBasename =
+  baseUrl === "/" ? undefined : baseUrl.replace(/\/$/, "");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <ScrollToTop />
       <HelmetProvider>
         <App />
